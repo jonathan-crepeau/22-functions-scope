358f991ec69fa044950bf0c4f6574061123c11a1
jonathan-crepeau <jon.crepeau@gmail.com>
2022-01-30 21:31:44 -0800

    Commit 2 - Palindrome

--- js/app.js
+++ js/app.js
@@ -1,2 +1,28 @@
 // console.log('Fear is the mind killer.');
 
+// SECTION VERBAL QUESTIONS:
+
+// 1. What is the difference between a parameter and an argument?
+
+  // Answer: The parameter is how the input is represented in the function when it is declared / defined. The argument is the input when the function is invoked/called.
+
+// 2. Within a function, what is the difference between return and console.log?
+
+    // Answer: Return stops the function and gives the function its value (provides the return value).
+
+// 3. What are the implications of the ability of a function to return a value?
+
+  // Answer: A return statement is needed for a function's OUTPUT VALUE to be passed as data, not console.log. 
+
+
+// SECTION PALINDROME AGAIN:
+
+const palindrome = (string) => {
+  reverse = string.split('').reverse().join('').toLowerCase();
+  return string.toLowerCase() === reverse ? true : false;
+
+  // if (string.toLowerCase() === reverse)
+  //   return true;
+  // else
+  // return false;
+}
